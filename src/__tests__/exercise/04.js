f6169b13ddbb41b5527f31c1771d8b226f202025
joonmo-you <joonmo.you@gmail.com>
2021-12-10 14:06:53 +0900

    04-extra4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -6,14 +6,14 @@ import {getByLabelText, render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-function buildLoginForm(overrides) {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides,
-  }
-}
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const {username, password} = buildLoginForm()
